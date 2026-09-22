1631b347ae5034b5c51005b9d2c868e244905baa
saurabh8898 <102570325+saurabh8898@users.noreply.github.com>
2026-09-22 13:27:06 +0530

    added app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-this is a java file
+from flask import Flask
+
+app = Flask(**name**)
+
+@app.route("/")
+def hello():
+return "DevSecOps scanner test application"
+
+@app.route("/health")
+def health():
+return "OK"
+
+if **name** == "**main**":
+app.run(host="0.0.0.0", port=5000)
